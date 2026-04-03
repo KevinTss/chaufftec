bb849d0d6c077852691060af54ce652ba3df41e2
Kevin Tassi <kevin.tassi@gmail.com>
2026-04-03 19:27:44 +0200

    Fix build

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,36 +9,36 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
-      alternates: {
-        languages: {
-          fr: `${SITE_URL}/fr`,
-          en: `${SITE_URL}/en`,
-        },
-      },
+    },
+    {
+      url: `${SITE_URL}/en`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
     },
     {
       url: `${SITE_URL}/fr/a-propos`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
-      alternates: {
-        languages: {
-          fr: `${SITE_URL}/fr/a-propos`,
-          en: `${SITE_URL}/en/about`,
-        },
-      },
+    },
+    {
+      url: `${SITE_URL}/en/about`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
     },
     {
       url: `${SITE_URL}/fr/services`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
-      alternates: {
-        languages: {
-          fr: `${SITE_URL}/fr/services`,
-          en: `${SITE_URL}/en/services`,
-        },
-      },
+    },
+    {
+      url: `${SITE_URL}/en/services`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
     },
   ]
 }
